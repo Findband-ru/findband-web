54813a215c890012382475b574b53f70aca6d41b
pavlukha <kinomant@yandex.ru>
2020-11-09 14:09:04 +0300

    WIP

--- src/next.config.js
+++ src/next.config.js
@@ -1,27 +1,32 @@
-const withFonts = require("next-fonts");
+// const withFonts = require("next-fonts");
 
-module.exports = withFonts({
+module.exports = {
   distDir: "../.next",
   webpack(config, options) {
-    config.node = {
-      fs: "empty",
-    };
-    config.module.rules.push({
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "url-loader?limit=100000",
-        },
-        {
-          loader: "file-loader",
-        },
-      ],
-    });
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
   },
-});
+};
+// module.exports = withFonts({
+//   distDir: "../.next",
+//   webpack(config, options) {
+//     config.node = {
+//       fs: "empty",
+//     };
+//     config.module.rules.push({
+//       test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+//       use: [
+//         options.defaultLoaders.babel,
+//         {
+//           loader: "url-loader?limit=100000",
+//         },
+//         {
+//           loader: "file-loader",
+//         },
+//       ],
+//     });
+// return config;
+// });
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,17 +14,17 @@ function MyApp({ Component, pageProps, classes }) {
   const [pageType, setPageType] = useState(0);
   const [isOnBoard, setIsOnboard] = useState(false);
 
-  const fontFamily = {
-    fontFamily: "AvertaCyrillicBlack",
-    fontStyle: "normal",
-    fontDisplay: "swap",
-    fontWeight: 400,
-    src: `
-      url('/fonts/AvertaCyrillicOtf') format('otf')
-      `,
-    unicodeRange:
-      "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
-  };
+  // const fontFamily = {
+  //   fontFamily: "AvertaCyrillicBlack",
+  //   fontStyle: "normal",
+  //   fontDisplay: "swap",
+  //   fontWeight: 400,
+  //   src: `
+  //     url('/fonts/AvertaCyrillicOtf') format('otf')
+  //     `,
+  //   unicodeRange:
+  //     "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+  // };
 
   const onChange = (user) => {
     if (user) {
@@ -44,12 +44,12 @@ function MyApp({ Component, pageProps, classes }) {
 
   return (
     <div className={classes.container}>
-      <style jsx global>{`
+      {/* <style jsx global>{`
         body {
           font-family: AvertaCyrillicBlack, sans-serif;
           overflow: hidden;
         }
-      `}</style>
+      `}</style> */}
       <Head>
         <title>Findband</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
